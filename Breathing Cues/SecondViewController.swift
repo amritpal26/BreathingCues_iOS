c9d17bb5f4bba030b5262155b5a2fbb5573eacab
Amritpal Singh <amritpal.am26@gmail.com>
2018-08-22 13:12:26 -0700

    Initial Commit

---
+++ Breathing Cues/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Breathing Cues
+//
+//  Created by Amritpal Singh on 2018-08-22.
+//  Copyright © 2018 Amritpal Singh. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
