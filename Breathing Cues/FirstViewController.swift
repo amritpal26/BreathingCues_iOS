96041fbc5d9e437eaa245302897acb307c97a608
Amritpal Singh <amritpal.am26@gmail.com>
2018-08-23 11:41:36 -0700

    Initial Commit

--- Breathing Cues/FirstViewController.swift
+++ Breathing Cues/FirstViewController.swift
@@ -9,17 +9,91 @@
 import UIKit
 
 class FirstViewController: UIViewController {
+    
+    let timerSecondsKey = "TimerSeconds"
+    let holdSecondsKey = "SecondListContinousHold"
+    let actionSecondsKey = "SecondListContinous"
+    
+    var timerSeconds: [Int] = []
+    var holdSeconds: [Int] = []
+    var actionSeconds: [Int] = []
+    
+    let userDefaults = UserDefaults.standard
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        loadData()
+//        setupPickerViews()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func loadData(){
+        let path = Bundle.main.path(forResource:"pickerData", ofType: "plist")
+        let dict:NSDictionary = NSDictionary(contentsOfFile: path!)!
+    
+        if (dict.object(forKey: "TimerSeconds") != nil) {
+            if let secArray = dict.object(forKey: "TimerSeconds") as? [Int] {
+                timerSeconds = secArray
+                print(secArray)
+            }
+        }
+        if (dict.object(forKey: "SecondListContinous") != nil) {
+            if let secArray = dict.object(forKey: "SecondListContinous") as? [Int] {
+                actionSeconds = secArray
+                print(secArray)
+            }
+        }
+        if (dict.object(forKey: "SecondListContinousHold") != nil) {
+            if let secArray = dict.object(forKey: "SecondListContinousHold") as? [Int] {
+                holdSeconds = secArray
+                print(secArray)
+            }
+        }
+    }
 }
 
+extension FirstViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        if pickerView.tag == 0{
+            return 4
+        }else{
+            return 1
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        if pickerView.tag == 0{
+            if component == 0 || component == 2{
+                return actionSeconds.count
+            }
+            else{
+                return holdSeconds.count
+            }
+        }else{
+            return timerSeconds.count
+        }
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        if pickerView.tag == 0{
+            if component == 0 || component == 2{
+                return String(actionSeconds[row])
+            }
+            else{
+                return String(holdSeconds[row])
+            }
+        }else{
+            return String(timerSeconds[row])
+        }
+    }
+}
+
+extension FirstViewController: UIPickerViewDelegate{
+    
+}
--- Breathing Cues/SecondViewController.swift
+++ Breathing Cues/SecondViewController.swift
@@ -7,19 +7,144 @@
 //
 
 import UIKit
+import UICircularProgressRing
 
-class SecondViewController: UIViewController {
+class SecondViewController: UIViewController{
+    
+    var timerSeconds: [Int] = []
+    var selectedTimer: Int = 30
+    var remainingTime: Int = 30
+    var isTimerRunning = false
+    var selectedRow = 0
+    var timer = Timer()
 
+    @IBOutlet weak var timerValue: UIButton!
+    @IBOutlet weak var timerLabel: UILabel!
+    @IBOutlet weak var progressView: UICircularProgressRing!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        loadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func PauseBtn(_ sender: Any) {
+        if isTimerRunning{
+            timer.invalidate()
+            isTimerRunning = false
+            timerValue.isEnabled = true
+            progressView.maxValue = 100
+            progressView.value = 100
+        }
+    }
+    
+    @IBAction func StartBtn(_ sender: Any) {
+        if !isTimerRunning{
+            runTimer()
+            timerValue.isEnabled = false
+            progressView.maxValue = CGFloat(selectedTimer)
+            progressView.value = CGFloat(selectedTimer)
+        }
+    }
+    
+    @IBAction func stopBtn(_ sender: Any) {
+        if isTimerRunning{
+            timer.invalidate()
+            remainingTime = selectedTimer
+            isTimerRunning = false
+            timerLabel.text = "0:00"
+            timerValue.isEnabled = true
+            progressView.maxValue = CGFloat(selectedTimer)
+            progressView.value = CGFloat(selectedTimer)
+        }
+    }
+    
+    @IBAction func timerPick(_ sender: Any) {
+        let alert = UIAlertController(title: "Timer", message: "\n\n\n\n\n\n", preferredStyle: .alert)
+        alert.isModalInPopover = true
+        
+        let pickerFrame = UIPickerView(frame: CGRect(x: 5, y: 20, width: 250, height: 140))
+        
+        alert.view.addSubview(pickerFrame)
+        pickerFrame.dataSource = self
+        pickerFrame.delegate = self
+        pickerFrame.selectRow(selectedRow, inComponent: 0, animated: false)
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { (UIAlertAction) in
+            print("You selected " + String(self.selectedTimer))
+            self.selectedTimer = self.timerSeconds[self.selectedRow]
+            self.remainingTime = self.selectedTimer
+            self.timerValue.setTitle(convertToString(totalSeconds: self.selectedTimer), for: .normal)
+        }))
+        self.present(alert,animated: true, completion: nil )
+    }
+    
+    func loadData(){
+        let path = Bundle.main.path(forResource:"pickerData", ofType: "plist")
+        let dict:NSDictionary = NSDictionary(contentsOfFile: path!)!
+        
+        if (dict.object(forKey: "TimerSeconds2") != nil) {
+            if let secArray = dict.object(forKey: "TimerSeconds2") as? [Int] {
+                timerSeconds = secArray
+                print(secArray)
+            }
+        }
+    }
+    
+    func runTimer(){
+        print("YEs")
+        isTimerRunning = true
+        timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: (#selector(SecondViewController.updateTimer)), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer(){
+        if remainingTime > 0{
+            remainingTime -= 1
+            print(String(remainingTime) + "")
+            let timeString = convertToString(totalSeconds: remainingTime)
+            timerLabel.text = timeString
+            progressView.value = CGFloat(remainingTime)
+        }else{
+            timer.invalidate()
+            isTimerRunning = false
+            remainingTime = selectedTimer
+        }
+    }
 }
 
+extension SecondViewController: UIPickerViewDataSource{
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return timerSeconds.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return String(timerSeconds[row])
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int){
+        selectedRow = row
+    }
+}
+extension SecondViewController: UIPickerViewDelegate{
+    
+}
+
+func convertToString(totalSeconds: Int) -> String {
+    let minutes = totalSeconds / 60
+    let seconds = totalSeconds % 60
+    if seconds > 9{
+        return String(minutes) + ":" + String(seconds)
+    }else{
+        return String(minutes) + ":0" + String(seconds)
+    }
+}
